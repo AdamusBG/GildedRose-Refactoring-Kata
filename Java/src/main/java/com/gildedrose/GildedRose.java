b4a8cf145b246bb3b4b62d540abd612a15f4439c
Adam Grieve <adamwizzy@hotmail.co.uk>
2021-01-14 17:30:30 +0000

    refactored originl code

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,54 +9,83 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+          if (items[i].name.equals("Aged Brie")) {
+            this.updateAgedBrie(items[i]);
+          } else if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            this.updateSulfuras(items[i]);
+          } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            this.updateBackstagePass(items[i]);
+          } else if (items[i].name.equals("Conjured")) {
+            this.updateConjured(items[i]);
+          } else {
+            this.updateNormal(items[i]);
+          }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+          if (items[i].quality > 50) {
+            this.reduceToMaxQuality(items[i]);
+          }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+          if (items[i].quality < 0) {
+            this.raiseToMinQuality(items[i]);
+          }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+          this.reduceSellIn(items[i]);
         }
+    }
+
+    private void updateAgedBrie(Item item) {
+      if (item.sellIn > 0) {
+        item.quality += 1;
+      }
+      if (item.sellIn <= 0) {
+        item.quality += 2;
+      }
+    }
+
+    private void updateSulfuras(Item item) {
+      item.quality = 80;
+    }
+
+    private void updateBackstagePass(Item item) {
+      if (item.sellIn >= 11) {
+        item.quality += 1;
+      } else if (item.sellIn >= 6) {
+        item.quality += 2;
+      } else if (item.sellIn >= 1) {
+        item.quality += 3;
+      } else {
+        item.quality = 0;
+      }
+    }
+
+    private void updateConjured(Item item) {
+      for (int i = 0; i < 2; i++) {
+        this.updateNormal(item);
+      }
+    }
+
+    private void updateNormal(Item item) {
+      if (item.sellIn > 0) {
+        item.quality -= 1;
+      }
+      if (item.sellIn <= 0) {
+        item.quality -= 2;
+      }
+    }
+
+    private void reduceToMaxQuality(Item item) {
+      if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        item.quality = 50;
+      }
+    }
+
+    private void raiseToMinQuality(Item item) {
+      item.quality = 0;
+    }
+
+    private void reduceSellIn(Item item) {
+      if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        item.sellIn -= 1;
+      }
     }
 }
